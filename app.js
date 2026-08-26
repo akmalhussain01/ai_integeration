3b8e1cb0d06e481391aeacdd809737340cbb832d
akmalhussain01 <akmalhuss313@gmail.com>
2026-08-26 19:35:20 +0500

    add the loging the tools calling g

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const groq = new Groq();
 
 
 async function main() {
+
   const completion = await groq.chat.completions.create({
     model: "openai/gpt-oss-120b",
     temperature: 0,
@@ -18,16 +19,59 @@ async function main() {
     // max_completion_tokens: 100,
     // response_format:{type:"json_object"},
     messages: [
-        {
-          role: "system",
-          content: "you are a smart  personal assistant and give me the answers for the asked questions and the answer should be short  ",
-        },
+      {
+        role: "system",
+        content: `you are a smart  personal assistant and give me the answers for the asked questions and the answer should be short.
+        you have access the following tools 
+        1.webSearch: Use this tool to get the latest information and real time data from the internet. You should only use this tool when you need to get the latest information and real time data from the internet.
+        `,
+      },
       {
         role: "user",
         content: "what is the latest model of claude code now ",
       },
     ],
+
+    tools: [
+      {
+        "type": "function",
+        "function": {
+          "name": "webSearch",
+          "description": "Use this tool to get the latest information and real time data from the internet. You should only use this tool when you need to get the latest information and real time data from the internet.",
+          "parameters": {
+            // JSON Schema object
+            "type": "object",
+            "properties": {
+              "query": {
+                "type": "string",
+                "description": "The search query to get the latest information and real time data from the internet."
+              }
+            },
+            "required": ["query"]
+          }
+        }
+      }
+    ],
+    tool_choice: "auto"
   });
-  console.log(completion.choices[0].message.content);
+
+
+  const toolCall = completion.choices[0].message.tool_calls;
+  if (!toolCall) {
+    console.log(completion.choices[0].message.content);
+    return;
+  }
+
+  for(const tool of toolCall){
+    console.log(`tools:${JSON.stringify(tool)}`)
+  }
+
+
+
 }
 main()
+
+const websearch = async ({ query }) => {
+  return "claude lastest model is fable 5"
+}
+
