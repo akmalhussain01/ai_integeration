c62b9b0649b9688c7b9066beff118de3eac8634a
akmalhussain01 <akmalhuss313@gmail.com>
2026-08-26 18:52:30 +0500

    add the tavily api to create a tool calling

--- app.js
+++ app.js
@@ -9,22 +9,22 @@ const groq = new Groq();
 
 async function main() {
   const completion = await groq.chat.completions.create({
-    model: "groq/compound",
-    // temperature: 0.7,
+    model: "openai/gpt-oss-120b",
+    temperature: 0,
     // top_p: 0.8,
     // stop:'ne',
     // frequency_penalty: 0.5,
     // presence_penalty: 0.5,
     // max_completion_tokens: 100,
-    response_format:{type:"json_object"},
+    // response_format:{type:"json_object"},
     messages: [
         {
           role: "system",
-          content: "jarvis , a smart  personal assistant give me the response in json format and in a single word ",
+          content: "you are a smart  personal assistant and give me the answers for the asked questions and the answer should be short  ",
         },
       {
         role: "user",
-        content: "who are you ?",
+        content: "what is the latest model of claude code now ",
       },
     ],
   });
