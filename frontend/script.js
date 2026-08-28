b7fe4a6073b1552b2825b0420f6fc6f90e15af77
akmalhussain01 <akmalhuss313@gmail.com>
2026-08-28 20:27:17 +0500

    add the frontend flies

---
+++ frontend/script.js
@@ -0,0 +1,26 @@
+const userInput = document.getElementById("input")
+
+document.addEventListener('keyup', handleEnter)
+
+function handleEnter(event) {
+
+
+    if (event.key === "Enter" && !event.shiftKey) {
+        const usertext = userInput.value.trim()
+        if (!usertext) {
+            return;
+        }
+        console.log("User input:", usertext)
+    }
+
+    generate(usertext)
+
+}
+
+function generate(usertext) {
+    const msg = document.createElement("div")
+    msg.className = `max-w-3xl mx-auto px-3 sm:px-4 py-4 sm:py-6 flex flex-col gap-4 sm:gap-6`
+    msg.textContent = usertext
+    document.getElementById("chat").appendChild(msg)
+    userInput.value = ""
+}
